a4d097897c71e1b15b712a5e5a9268dd4d53d1d2
Jaimedgp <jaime.diez.gp@gmail.com>
2021-02-22 14:02:53 +0100

    Download data from the 3 nearest stations

--- src/Curation/AEMET/main.py
+++ src/Curation/AEMET/main.py
@@ -32,27 +32,29 @@ def download_nearest_data(df_row):
                                                  long=df_row["longitude"],
                                                  near=3)
 
-    for st in nearest_station["indicativo"].values:
+    for i, (st, lat, long) in enumerate(
+                                    zip(nearest_station["indicativo"].values,
+                                        nearest_station["latitud"].values,
+                                        nearest_station["longitud"].values)):
+
         all_data = Aemet.get_data(dates=[date(2013, 1, 1),
                                          date(2020, 12, 31)],
                                   station_id=st,
                                   )
+
         if all_data is not None:
-            break
-    else:
-        return None
+            all_data["siteAQ"] = df_row["site"]
+            all_data["latitud"] = lat
+            all_data["longitud"] = long
 
-
-    all_data["siteAQ"] = df_row["site"]
-
-    all_data.to_csv(HOME+"data/Curation/AEMET/"+df_row["site"]+".csv",
-                    index=False)
+            all_data.to_csv(HOME+"data/Curation/AEMET/Values/%s-%s.csv"
+                            %(df_row["site"], i), index=False)
 
 
 # ----------------------------------------
 #     ESTACIONES CALIDAD DEL AIRE
 # ----------------------------------------
 
-sites_AQ = pd.read_csv(HOME+"data/Curation/sitesAQ.csv")
+sites_AQ = pd.read_csv(HOME+"data/Curation/AirQuality/sitesAQ.csv")
 
 sites_AQ.apply(download_nearest_data, axis=1)
