193c4e5265a9b01b79097250c03c42820d0e8903
Jaimedgp <jaime.diez.gp@gmail.com>
2021-02-23 10:41:56 +0100

    mss.yr and hv.mis conditions separate

--- src/Curation/AirQuality/main.R
+++ src/Curation/AirQuality/main.R
@@ -21,9 +21,9 @@ source("src/Curation/AirQuality/sitesAQ.R")
 source("src/Curation/AirQuality/info_sitesAQ.R")
 
 
-#----------------------------------------------------
+#########################################
 # Variables Principales del Estudio
-#----------------------------------------------------
+#########################################
 
 start_dt <- ymd_hms("2013-01-01 00:00:00")
 end_dt <- ymd_hms("2020-12-30 00:00:00")
@@ -31,9 +31,9 @@ end_dt <- ymd_hms("2020-12-30 00:00:00")
 pollutants <- c("no", "no2", "o3", "pm10", "pm2.5")
 
 
-#----------------------------------------------------
+##############################
 # Ejecucion del Programa
-#----------------------------------------------------
+##############################
 
 # Obtencion de las estaciones de estudio
 sitesAQ <- get.spain.sites(start_dt)
@@ -48,14 +48,26 @@ all.info <- do.call(rbind.fill,
                                               info_sitesAQ,
                                               st, start_dt, end_dt))
                            }, pollutants))
+all.info  <- mutate_if(all.info, is.factor, as.character)
+write.csv(all.info,
+          "data/Curation/AirQuality/info_sitesAQ.csv", row.names=FALSE)
 
+#--------------------------------------
 # Filtrar con los datos validos
+#--------------------------------------
+
 valid.info <- all.info[all.info$hv.min == TRUE &
-                       !is.na(all.info$hv.min) &
-                       all.info$mss.yr < 5
+                       !is.na(all.info$hv.min)
                       ,]
 
+valid.info <- valid.info[valid.info$mss.yr < 5,]
+
+write.csv(sitesAQ[sitesAQ$site %in% levels(as.factor(valid.info$site)),],
+          "data/Curation/AirQuality/sitesAQ.csv", row.names=FALSE)
+
+#-------------------
 # descargar datos
+#-------------------
 for (st in levels(as.factor(valid.info$site))) {
 
     data.st <- do.call(rbind,
