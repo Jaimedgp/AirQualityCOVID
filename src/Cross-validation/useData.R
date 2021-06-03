5ea0b597273d94aab0bef91e96c1ecad5cfe141d
Jaimedgp <jaime.diez.gp@gmail.com>
2021-06-03 15:04:45 +0200

    [Solved] add.yesterday.meteo even for various sites, municipios or variables

--- src/Cross-validation/useData.R
+++ src/Cross-validation/useData.R
@@ -35,11 +35,7 @@ open.data <- function(sites,
                            date > start_dt,
                            date < end_dt) %>%
                     pivot_wider(names_from = variable,
-                                values_from = value) %>%
-                    openair::timeAverage(avg.time = "day",
-                                        type="site") %>%
-                    mutate(date = lubridate::as_date(date)) %>%
-                    select(-date_end, -process, -summary, -validity)
+                                values_from = value)
 
         aq[is.na(aq)] <- NA
 
@@ -81,6 +77,17 @@ add.yesterday.meteo <- function(dat.df, n.days=1) {
     if (!("date" %in% names(dat.df))) {
         print("Must have a date column")
         return()
+    } else {
+        by.col <- c("date")
+    }
+    if("site" %in% names(dat.df)){
+        by.col <- c(by.col, "site")
+    }
+    if("variable" %in% names(dat.df)){
+        by.col <- c(by.col, "variable")
+    }
+    if("Municipio" %in% names(dat.df)){
+        by.col <- c(by.col, "Municipio")
     }
 
     meteo.cl <- c("ws", "wd", "tmed", "prec", "tmin", "tmax",
@@ -91,13 +98,12 @@ add.yesterday.meteo <- function(dat.df, n.days=1) {
 
     for (i in 1:n.days) {
         new.df <- dat.df %>%
-                mutate(date=date-i) %>%
-                select(date, all_of(c("ws", "wd", "tmed", "prec", "tmin", "tmax",
-                                    "presMax", "presMin", "RH", "solar.radiation")))
+                select(all_of(by.col), all_of(col.add)) %>%
+                mutate(date=date+i)
 
         yesterday.df <- merge(yesterday.df, new.df,
-                              by = "date",
-                              all.x=T,
+                              by = by.col,
+                              all.x=F, all.y=T,
                               suffixes = c("", paste("", i, "before", sep="."))
                               )# %>% slice(-1)
     }
