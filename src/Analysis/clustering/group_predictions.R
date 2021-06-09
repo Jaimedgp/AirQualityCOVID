1d0da23049ef1610d3ca1f48d7a9541737cb9d5d
Jaimedgp <jaime.diez.gp@gmail.com>
2021-06-09 17:10:37 +0200

    clustering analysis

---
+++ src/Analysis/clustering/group_predictions.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+setwd("~/Repositories/AirQualityCOVID/")
+source("src/Analysis/functions.R")
+
+data.df <- merge(read.csv("data/Results/predictions.csv"),
+                 read.csv("data/Curation/checked_AQ.csv"),
+                 by = c("site", "variable"),
+                 all.x = T, all.y=F
+                ) %>%
+            filter(variable == "no2") %>%
+            group_by(Municipio, variable, date) %>%
+            summarise(obs = mean(obs, na.rm=T),
+                      pred = mean(pred, na.rm=T)
+                     ) %>%
+            mutate(diff = relative.change(obs, pred))
+
+write.csv(data.df,
+          "data/Results/predictions_municipios.csv",
+          row.names=F)
